69e7bed90dbd04f03f7ac94f59181548f162bab5
yash <yash.dhimar@tristatetechnology.co.in>
2023-07-18 10:17:22 +0530

    changes

--- src/v1/api/services/FeelingService.js
+++ src/v1/api/services/FeelingService.js
@@ -4,26 +4,28 @@ class FeelingService {
   async create(body, userId) {
     return new Promise(async (resolve, reject) => {
       try {
-        let {type,videoId } = body
-        console.log("body=========>",body)
-        var video = await Video.find({ _id: videoId })
-        if (video.length > 0) {
-          var feeling = await Feeling.create({ type, videoId, userId: userId })
-          resolve(feeling)
+        let { type, videoId } = body
+        console.log("body=========>", body)
+        var data = await Feeling.find({ userId: userId })
+        if (data.length > 0) {
+          var dlt_like = await Feeling.updateOne({ userId: userId }, { $set: { type: 'blank' } })
+          resolve()
         } else {
-          var err = { message: "ID NOT FOUND PLEASE ENTER VALID ID" }
-          reject(err)
+          var video = await Video.find({ _id: videoId })
+          if (video.length > 0) {
+            var feeling = await Feeling.create({ type, videoId, userId: userId })
+            resolve(feeling)
+          } else {
+            var err = { message: "ID NOT FOUND PLEASE ENTER VALID ID" }
+            reject(err)
+          }
         }
-
-         var data = await Feeling.find({userId:userId})
-         if(data.length>0){
-          var dlt_like = await Feeling.deleteOne({})
-         }
-
       } catch (error) {
         return reject(error)
       }
     })
-  }
+  }   
 }
 module.exports = new FeelingService()
+
+
